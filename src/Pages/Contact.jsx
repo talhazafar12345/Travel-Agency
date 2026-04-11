2cc7380f69e92e7c3413069c3e6a6b7230d1d661
Talha Zafar <talhazafarmustafa@gmail.com>
2026-04-11 14:25:06 +0500

    File Updated

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,6 @@
 import Hero1 from "../Sections/Hero1"
 import Hero19 from "../Sections/Hero19"
+import Hero20 from "../Sections/Hero20"
 import Hero28 from "../Sections/Hero28"
 import Hero29 from "../Sections/Hero29"
 import { useEffect } from "react"
@@ -19,6 +20,7 @@ window.scrollTo(0,0)
     <div className="Faqs-hero27">
     <Hero19 />
     </div>
+    <Hero20 />
       
     </div>
   )
